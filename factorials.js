61d364de7afcb861646d937da6d46022f2a6847b
khorramk <khorramk.kbsk@gmail.com>
2017-12-06 21:32:01 +0000

    Create factorials.js

---
+++ factorials.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+
+/*var sol = 12;
+
+while (sol >= 1){
+ sol -= 1;
+ 
+  
+}*/
+var solution = 1;
+for (var sol=12; sol > 0; sol--){
+  solution *= sol;
+  
+}
+
+console.log(solution);
